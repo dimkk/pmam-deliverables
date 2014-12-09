4532c96975cef64cffde063e94f489c63dac1901
scahall <seancahall@gmail.com>
2014-12-09 14:58:33 -0800

    updates to deliverables controller

--- app/modules/deliverables/controllers/deliverablesController.js
+++ app/modules/deliverables/controllers/deliverablesController.js
@@ -8,6 +8,24 @@
     /* @ngInject */
     function deliverablesController( $location, $scope, toastr, $state, deliverablesService ) {
 
+        var fy = $state.params.fy || '2013';
+        var mo = $state.params.mo || '7';
+
+        var currentFiscalYear = 0;
+        var currentMonth = 0;
+        var deliverableDefinitions;
+
+        $scope.gotData = false;
+
+        var monthNames = ["SEP","OCT","NOV","DEC","JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG"];
+
+        var fiscalYear = fy.substr(fy.length - 2);
+
+        if( mo < 0 ) {
+            mo = 11;
+        }
+
+        $scope.deliverableFrequencyFilter = deliverableFrequencyFilter;
         $scope.decreaseDate = decreaseDate;
         $scope.increaseDate = increaseDate;
 
@@ -17,21 +35,48 @@
 
         function activate() {
 
-            deliverablesService.getDeliverables().then(
-                function(results) {
+            deliverablesService.getDeliverablesForMonth( fy, mo ).then(
+
+                function( results ) {
+
                     $scope.deliverablesByMonth = results;
-                    $scope.displayDate = results.state.displayDate;
-                    $scope.state = results.state;
+
+                    if($scope.deliverablesByMonth[0] !== undefined) {
+
+                        var fiscalYear = $scope.deliverablesByMonth[0].fy;
+                        var yearPart = fiscalYear.substr(fiscalYear.length - 2);
+                        $scope.fiscalYear = fiscalYear;
+                        $scope.displayPeriod = monthNames[$scope.deliverablesByMonth[0].month] + " " + yearPart;
+                        currentMonth = $scope.deliverablesByMonth[0].month;
+                    }
+                    // this is where we handle increments and decrements when there's no data
+                    else {
+
+                        if( mo > 11 ){
+                            mo = 0;
+                        }
+                        if( mo < 0 ){
+                            mo = 11;
+                        }
+
+                        $scope.displayPeriod = monthNames[ mo ] + " " + fy.substr(fy.length - 2);
+
+                    }
+
                 },
                 function(err) {
                     console.log(err);
                 }
             );
 
-            deliverablesService.getDeliverableDefinitions().then(
-                function(results) {
-                    $scope.deliverableDefinitions = results;
-                    debugger;
+            deliverablesService.getDeliverableDefinitionsForMonth( fy, mo ).then(
+
+                function( results ) {
+
+                    $scope.gotData = true;
+                    $scope.deliverableDefinitionsByMonth = results.deliverableDefinitionsByMonth;
+                    deliverableDefinitions = results.deliverableDefinitions;
+
                 },
                 function(err) {
                     console.log(err);
@@ -39,9 +84,10 @@
             );
 
             deliverablesService.getDeliverableFrequencies().then(
+
                 function(results) {
                     $scope.deliverableFrequencies = results;
-                    debugger;
+
                 },
                 function(err) {
                     console.log(err);
@@ -50,54 +96,59 @@
 
         }
 
-        function getDeliverablesByMonth() {
+        function deliverableFrequencyFilter( deliverableType ) {
 
-            var fy = $state.fy || '2013';
+            var deliverableDefinition = deliverableDefinitions[ deliverableType.lookupId ];
 
-            deliverablesService.getDeliverables().then(function(results) {
+            if( deliverableDefinition ) {
 
-                $scope.allMyDeliverables = results;
-                console.log(results);
+                return deliverableDefinition.frequency.lookupValue;
+            }
 
-            });
         }
 
+
+
+        // 9/1 starts the new fiscal year
         function increaseDate() {
-            var currentMonthIndex = state.availableMonths.indexOf(state.displayDate);
-            if (currentMonthIndex === 0) {
-                toastr.info("There are no newer records to display.")
-            } else if (currentMonthIndex === -1) {
-                toastr.error("There was a problem with your request.  Please try again.")
-            } else {
-                state.displayDate = state.availableMonths[currentMonthIndex - 1];
+
+            currentMonth = parseInt(mo) + 1;
+            currentFiscalYear = fy;
+
+            // if we're flipping to the new year, increment current fiscal year bucket
+            if( currentMonth === 4 ) {
+                currentFiscalYear = parseInt(currentFiscalYear) + 1;
+                currentFiscalYear = currentFiscalYear.toString();
             }
+
+            // fetch the filtered data
+            $location.search( 'fy', currentFiscalYear );
+            $location.search( 'mo',  currentMonth );
+
         }
 
-        function openDeliverablesForm(deliverable) {
-            if (deliverable) {
-                $stateParams.id = deliverable.id;
-                $location.path("/deliverable/" + $stateParams.id);
-            }
-            else {
-                $location.path("/deliverable");
-            }
-        }
 
         function openDefinitionsForm(deliverable) {
             $location.path("/definitions");
         }
 
         function decreaseDate() {
-            var currentMonthIndex = state.availableMonths.indexOf(state.displayDate);
-            if (currentMonthIndex === (state.availableMonths.length - 1)) {
-                toastr.info("There are no older records to display.")
-            } else if (currentMonthIndex === -1) {
-                toastr.error("There was a problem with your request.  Please try again.")
-            } else {
-                state.displayDate = state.availableMonths[currentMonthIndex + 1];
+
+            currentMonth = parseInt(mo) - 1;
+            currentFiscalYear = fy;
+
+            // if we're flipping to the previous year, decrement current fiscal year bucket
+            if( currentMonth === 3 ) {
+                currentFiscalYear = parseInt(currentFiscalYear) - 1;
+                currentFiscalYear = currentFiscalYear.toString();
             }
+
+            if( currentMonth === 0 ){
+                currentMonth = 12;
+            }
+
+            $location.search( 'fy', currentFiscalYear );
+            $location.search( 'mo', currentMonth );
         }
-
-
     }
 })();
