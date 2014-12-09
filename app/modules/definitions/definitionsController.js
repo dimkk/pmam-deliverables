70e5f2327d99f6cf7e13a5e234d569adead7d3f2
scahall <seancahall@gmail.com>
2014-12-09 14:54:09 -0800

    controller and view for definition types

---
+++ app/modules/definitions/definitionsController.js
@@ -0,0 +1,44 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('pmam-deliverables')
+        .controller('definitionsController', definitionsController);
+
+    /* @ngInject */
+    function definitionsController($state, $scope, deliverableDefinitionsModel, deliverablesService) {
+
+        var fy = $state.params.fy || '2013';
+
+        $scope.fiscalYear = fy;
+        $scope.gotData = false;
+        $scope.state = {
+            definitions: [],
+            validChartData: 'false',
+            searchString: ''
+        };
+
+        activate();
+
+
+        /**==================PRIVATE==================*/
+
+        function activate() {
+
+            deliverableDefinitionsModel.getFyDefinitions(fy)
+                .then(function (result) {
+
+                    $scope.deliverableDefinitions = result.toArray();
+                    $scope.state.validChartData = true;
+
+                }),
+                function(err) {
+                    console.log(err);
+                }
+
+            deliverablesService.getDeliverableCountByDefinition(fy).then(function(deliverableCountByDefinition){
+                $scope.deliverableCountByDefinition = deliverableCountByDefinition;
+            });
+        }
+    }
+})();
