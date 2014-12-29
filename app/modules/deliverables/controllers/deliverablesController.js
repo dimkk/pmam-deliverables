21798ac8b78102a5bbd402c51ad2e838c7d0e5ca
scahall <seancahall@gmail.com>
2014-12-29 13:23:27 -0800

    moving google charts logic to chart service

--- app/modules/deliverables/controllers/deliverablesController.js
+++ app/modules/deliverables/controllers/deliverablesController.js
@@ -40,27 +40,12 @@
         $scope.decreaseDate = decreaseDate;
         $scope.increaseDate = increaseDate;
 
-        var state = {
-            displayDate: 'loading',
-            selectedDivision: '',
-            selectedTeam: '',
-            showDivisions: false,
-            viewModeMonth: true,
-            displayMode: "displayDate",
-            title: "Deliverables",
-            monthActive: 'active',
-            qtrActive: null,
-            displayedTitle: '',
-            validChartData: false,
-            availableMonths: []
-        }
-
         activate();
 
         /**==================PRIVATE==================*/
 
         function activate() {
-            buildGauges();
+            doBuildGauges();
             deliverablesService.getDeliverablesForMonth( fy, mo ).then(
 
                 function( results ) {
@@ -125,98 +110,26 @@
 
         function initializeMetricsGauages() {
 
-            prepareMetrics();
+            var metricsData = doPrepareMetrics();
 
-            $scope.Gauge1.data.rows[0].c[1].v = getRandom();
-            $scope.Gauge2.data.rows[0].c[1].v = getRandom();
-            $scope.Gauge3.data.rows[0].c[1].v = getRandom();
+            // faking the gauge data for now; it should run off the values returned in metricsData
+            $scope.Gauge1.data.rows[0].c[1].v = chartService.getRandom();
+            $scope.Gauge2.data.rows[0].c[1].v = chartService.getRandom();
         }
+        
+        function doBuildGauges() {
 
-        function getRandom() {
-            return Math.floor(Math.random() * 5) + 1;
-        }
+            var gauges = chartService.buildGauges();
 
-        function buildGauges() {
             //Create initial gauge objects if not already defined
-            $scope.Gauge1 = new chartService.GaugeChart({
-                options: {
-                    animation: {
-                        easing: 'out',
-                        duration: 1000
-                    }
-                },
-                data: {
-                    rows: [
-                        {
-                            "c": [
-                                {"v": 'Satisfaction', "p": {}},
-                                {"v": 0, "p": {}}
-                            ]
-                        }
-                    ]
-                }
-            });
-            $scope.Gauge2 = new chartService.GaugeChart({
-                options: {
-                    animation: {
-                        easing: 'out',
-                        duration: 1000
-                    }
-                },
-                data: {
-                    rows: [
-                        {
-                            "c": [
-                                {"v": 'Health', "p": {}},
-                                {"v": 0, "p": {}}
-                            ]
-                        }
-                    ]
-                }
-            });
-            $scope.Gauge3 = new chartService.GaugeChart({
-                options: {
-                    animation: {
-                        easing: 'out',
-                        duration: 1000
-                    }
-                },
-                data: {
-                    rows: [
-                        {
-                            "c": [
-                                {"v": 'OTD', "p": {}},
-                                {"v": 0, "p": {}}
-                            ]
-                        }
-                    ]
-                }
-            });
+            $scope.Gauge1 = gauges.Gauge1;
+            $scope.Gauge2 = gauges.Gauge2;
         }
 
-        function prepareMetrics() {
-            $scope.metricsByMonth = {};
-            //Clear out any monthly data
-            state.availableMonths.length = 0;
-            //Add references to each metric broken out by date
-            console.log($scope.deliverablesByMonth);
-            _.each($scope.deliverablesByMonth, function (deliverable) {
+        function doPrepareMetrics() {
 
-                //Sets initial date to the most recent display date
-                state.displayDate = deliverable.displayDate;
+            $scope.metricsByMonth = chartService.prepareMetrics($scope.deliverablesByMonth);
 
-                console.log(state.displayDate);
-                //Create array to hold metrics for this month if it doesn't exist
-                $scope.metricsByMonth[deliverable[state.displayMode]] = $scope.metricsByMonth[deliverable[state.displayMode]] || []
-                $scope.metricsByMonth[deliverable[state.displayMode]].push(deliverable);
-
-            });
-            _.each($scope.metricsByMonth, function (monthMetrics, monthLabel) {
-                state.availableMonths.push(monthLabel);
-            });
-
-            console.log($scope.metricsByMonth);
-            state.validChartData = true;
         }
 
         function deliverableFrequencyFilter( deliverableType ) {
