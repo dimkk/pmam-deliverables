b92af1763af13a77def89d99b6fd611cc223ca3f
scahall <seancahall@gmail.com>
2015-01-22 13:55:14 -0800

    adding reviews directive and related calls

---
+++ app/modules/deliverables/directives/reviews.js
@@ -0,0 +1,32 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('pmam-deliverables')
+        .directive('reviews', reviews);
+
+    reviews.$inject = ['$window'];
+
+    /* @ngInject */
+    function reviews($window) {
+        // Usage:
+        //
+        // Creates:
+        //
+        var directive = {
+            link: link,
+            templateUrl: 'modules/deliverables/directives/reviewsView.html',
+            scope: {feedback: '='},
+            restrict: 'EA'
+        };
+        return directive;
+
+        function link(scope, element, attrs) {
+            var deliverableFeedback = scope.feedback;
+            scope.state.rating = deliverableFeedback.rating;
+            scope.state.created = deliverableFeedback.created;
+            scope.state.author = deliverableFeedback.author.lookupValue;
+            scope.state.comments = deliverableFeedback.comments;
+        }
+    }
+})();
