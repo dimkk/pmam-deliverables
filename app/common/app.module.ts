70c7f2fb1cc1b29274207fcb4146f041e2f66c7e
Scott Hatcher <scott@scotthatcher.com>
2015-04-23 11:55:16 -0700

    Removed unused modules.

--- app/common/app.module.ts
+++ app/common/app.module.ts
@@ -2,10 +2,10 @@ module app {
     'use strict';
 
     var modules = [    'ngSanitize', 'ngAnimate', 'ngMessages', 'googlechart', 'ui.router', 'ui.bootstrap',
-        'ui.date', 'ui.utils', 'ui.select', 'ui.sortable', 'ui.highlight', 'ui.grid', 'ui.grid.edit',
+        'ui.date', 'ui.utils', 'ui.select', 'ui.grid', 'ui.grid.edit',
         'ui.grid.cellNav', 'ui.grid.pinning', 'ui.grid.resizeColumns', 'ui.grid.moveColumns',
         'ui.grid.rowEdit', 'ui.grid.selection', 'ui.grid.autoResize', 'ui.grid.expandable', 'ngTable',
-        'firebase', 'toastr', 'angular-loading-bar', 'angular.filter', 'monospaced.elastic', 'angularPoint'
+        'firebase', 'toastr', 'angular-loading-bar', 'monospaced.elastic', 'angularPoint'
     ];
 
     var offline = false;
