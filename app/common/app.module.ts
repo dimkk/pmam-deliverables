2e3dab7ef39cac9629e76b0ea9e5ab7e69434465
Scott Hatcher <scott@scotthatcher.com>
2015-04-23 13:02:29 -0700

    Updated required dependencies.

--- app/common/app.module.ts
+++ app/common/app.module.ts
@@ -4,8 +4,9 @@ module app {
     var modules = [    'ngSanitize', 'ngAnimate', 'ngMessages', 'googlechart', 'ui.router', 'ui.bootstrap',
         'ui.date', 'ui.utils', 'ui.select', 'ui.grid', 'ui.grid.edit',
         'ui.grid.cellNav', 'ui.grid.pinning', 'ui.grid.resizeColumns', 'ui.grid.moveColumns',
-        'ui.grid.rowEdit', 'ui.grid.selection', 'ui.grid.autoResize', 'ui.grid.expandable', 'ngTable',
-        'firebase', 'toastr', 'angular-loading-bar', 'monospaced.elastic', 'angularPoint'
+        'ui.grid.rowEdit', 'ui.grid.selection', 'ui.grid.autoResize', 'ui.grid.expandable',
+        'firebase', 'toastr', 'angular-loading-bar', 'angular.filter', 'monospaced.elastic',
+        'angularPoint'
     ];
 
     var offline = false;
--- test/mocks/app.module.mock.js
+++ test/mocks/app.module.mock.js
@@ -2,10 +2,11 @@
     'use strict';
 
     var modules = [    'ngSanitize', 'ngAnimate', 'ngMessages', 'googlechart', 'ui.router', 'ui.bootstrap',
-        'ui.date', 'ui.utils', 'ui.select', 'ui.sortable', 'ui.highlight', 'ui.grid', 'ui.grid.edit',
+        'ui.date', 'ui.utils', 'ui.select', 'ui.grid', 'ui.grid.edit',
         'ui.grid.cellNav', 'ui.grid.pinning', 'ui.grid.resizeColumns', 'ui.grid.moveColumns',
-        'ui.grid.rowEdit', 'ui.grid.selection', 'ui.grid.autoResize', 'ui.grid.expandable', 'ngTable',
-        'firebase', 'toastr', 'angular-loading-bar', 'angular.filter', 'monospaced.elastic', 'angularPoint', 'ngMock'
+        'ui.grid.rowEdit', 'ui.grid.selection', 'ui.grid.autoResize', 'ui.grid.expandable',
+        'firebase', 'toastr', 'angular-loading-bar', 'angular.filter', 'monospaced.elastic',
+        'angularPoint', 'ngMock'
     ];
 
     angular.module('pmam-deliverables', modules);
