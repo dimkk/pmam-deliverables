715f994dd5ee218c79d5442cbd701609a76f05f9
Scott Hatcher <scott@scotthatcher.com>
2015-05-15 11:26:14 -0700

    Commit for Ryan.

---
+++ app/common/directives/definitionSelector.ts
@@ -0,0 +1,72 @@
+﻿/// <reference path="../../../typings/app.d.ts" />
+module app {
+    'use strict';
+    var vm: definitionSelectorController;
+    var template = `
+
+       <div class="form-group" >
+                <label>Type&nbsp;</label>
+                <select ng-model="vm.$scope.selectedDefinition"
+                        class="form-control"
+                        data-ng-options="deliverableDefinition.title for deliverableDefinition in vm.definitions | toArray | orderBy: 'title' track by deliverableDefinition.id"></select>
+            </div>
+                `;
+    // //data-ng-options="vm.dropdownLabel(deliverableDefinition) for deliverableDefinition in vm.definitions | toArray | orderBy: 'title' track by deliverableDefinition.id"></select>
+    function definitionSelector() {
+        return {
+            controller: definitionSelectorController,
+            controllerAs: 'vm',
+            scope: { selectedTask: '=', fiscalYear: '=', selectedDefinition: '='},
+            template: template
+        };
+    }
+    class definitionSelectorController {
+        definitions;
+        selectedDefinition: Object;
+        isVisible: boolean;
+        constructor(private $scope, private deliverableDefinitionsModel: DeliverableDefinitionsModel) {
+            vm = this;
+            
+            vm.isVisible = ($scope.visible !== undefined ? $scope.visible : true);
+
+
+            vm.definitions =deliverableDefinitionsModel.getDeliverableDefinitionsByTaskNumber($scope.fiscalYear, ($scope.selectedTask == 'All' ? undefined : $scope.selectedTask))
+                .then(function (resolvedPromise) {
+                vm.definitions = _.toArray(resolvedPromise);
+                
+               
+                var count = 0;
+                _.each(vm.definitions, (deliverableDefinition) => {
+                    if (deliverableDefinition.id === $scope.selectedDefinition.id)
+                        count += 1;
+                });
+
+                if (count === 0)
+                    $scope.selectedDefinition = vm.definitions[0];
+                 
+            });
+
+        }
+        
+        getUpdateState() {
+            
+            vm.$scope.selectedDefinition = vm.selectedDefinition;
+        }
+        dropdownLabel(deliverableDefinition) {
+            var deliverables = deliverableDefinition.getDeliverablesForDefinition();
+            return deliverableDefinition.title + ' (' + _.toArray(deliverables).length + ')';
+        }
+    }
+
+
+
+    angular
+        .module('pmam-deliverables')
+        .directive('definitionSelector', definitionSelector);
+
+}
+
+ 
+
+
+ 
---
+++ app/common/directives/fiscalSelector.ts
@@ -0,0 +1,71 @@
+﻿/// <reference path="../../../typings/app.d.ts" />
+module app {
+    'use strict';
+    var vm: fiscalSelectorController;
+    var template = `
+    <form class="navbar-form navbar-right" ng-if="vm.fy !== 'undefined'">
+            <div class="btn-group">
+                <button class="btn btn-default pull-left" type="button"  ng-click="vm.decrementFiscalDate()">
+                    <i class="fa fa-arrow-left"></i>
+                </button>
+                <span class="pull-left time-indicator text-muted">{{ vm.displayFiscal() }}</span>
+                <button class="btn btn-default pull-left" type="button" ng-click="vm.incrementFiscalDate()">
+                    <i class="fa fa-arrow-right"></i>
+                </button>
+            </div>
+    </form>
+                `;
+
+    function fiscalSelector() {
+        return {
+            controller: fiscalSelectorController,
+            controllerAs: 'vm',
+            scope: { fiscalMonth: '=', fiscalYear : '='},
+            template: template
+        };
+    }
+    class fiscalSelectorController {
+        
+        constructor(private $scope, private calendarService: CalendarService) {
+            vm = this;
+        }
+
+        decrementFiscalDate() {
+            var updatedMonth = (vm.$scope.fiscalMonth ? vm.$scope.fiscalMonth - 1 : 0);
+            var updatedYear = vm.$scope.fiscalYear;
+            // if we're flipping to the previous year, decrement current fiscal year bucket
+            if (updatedMonth === 0) {
+                updatedYear = updatedYear - 1;
+                updatedMonth = 12;
+            }
+            vm.$scope.fiscalYear = updatedYear;
+            vm.$scope.fiscalMonth = (vm.$scope.fiscalMonth ? updatedMonth : undefined);
+        }
+        displayFiscal() {
+            return (vm.$scope.fiscalMonth ? vm.calendarService.generateDisplayPeriod(vm.$scope.fiscalMonth, vm.$scope.fiscalYear) : 'FY ' + vm.$scope.fiscalYear.toString().slice(-2));
+           // return (vm.parentScope.fiscalMonth ? vm.cs.generateDisplayPeriod(vm.parentScope.fiscalMonth, vm.parentScope.fiscalYear) : 'FY ' + vm.parentScope.fiscalYear.toString().slice(-2));
+        }
+        incrementFiscalDate() {
+            var updatedMonth = (vm.$scope.fiscalMonth ? vm.$scope.fiscalMonth + 1 : 13);
+            var updatedYear = vm.$scope.fiscalYear;                        
+           
+            // if we're flipping to the next year, increment current fiscal year bucket
+            if (updatedMonth > 12) {
+                updatedYear += 1;
+                updatedMonth = 1;
+            }
+
+            vm.$scope.fiscalYear = updatedYear;
+            vm.$scope.fiscalMonth = (vm.$scope.fiscalMonth ? updatedMonth : undefined);
+        }
+    }
+
+    
+
+    angular
+        .module('pmam-deliverables')
+        .directive('fiscalSelector', fiscalSelector);
+
+}
+
+ 
