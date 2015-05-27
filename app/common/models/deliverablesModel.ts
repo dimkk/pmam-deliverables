59b812aaa2789eedec78b3a714d553b7a93080b5
Scott Hatcher <github@scotthatcher.com>
2015-05-27 14:22:47 -0700

    Bug fix to prevent async call for each record.

--- app/common/models/deliverablesModel.ts
+++ app/common/models/deliverablesModel.ts
@@ -241,9 +241,9 @@ module app {
             var deliverable = this;
             var acceptableCount = 0;
             var status = 'Not Rated';
-            deliverableFeedbackModel.getFyFeedback(deliverable.fy);
+            //deliverableFeedbackModel.getFyFeedback(deliverable.fy);
             var feedbackRecords = _.toArray(deliverable.getCachedFeedbackByDeliverableId());
-            
+
             /** Increment for each feedback marked as acceptable */
             _.each(feedbackRecords, function (feedbackRecord) {
                 if (feedbackRecord.acceptable) {
